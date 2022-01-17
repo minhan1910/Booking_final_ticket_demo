ecc4f2fc73fbf470689f4b966f75e2ff98130a9e
An <annogo123@gmail.com>
2022-01-17 10:03:52 +0700

    Udapte_branch_remove_all_import_in_appjs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import "./App.css";
-import BaiTapTongHop from "./BaiTapTongHop/BaiTapTongHop";
-import ChunkLodash from "./Lodash/ChunkLodash";
-import CompareObject from "./Lodash/CompareObject";
-import FlattrenLodash from "./Lodash/FlattrenLodash";
-import JoinDemo from "./Lodash/JoinDemo";
-import SortLodash from "./Lodash/SortLodash";
-import UniqLodash from "./Lodash/UniqLodash";
-import BaiTapTailWind from "./TailWindComponent/BaiTapTailWind";
-import BorderRadiusDemo from "./TailWindComponent/BorderRadiusDemo";
-import CustomCss from "./TailWindComponent/CustomCss";
-import DemoGrid from "./TailWindComponent/DemoGrid";
-import FlexDemo from "./TailWindComponent/FlexDemo";
-import PaddingMarginDemo from "./TailWindComponent/PaddingMarginDemo";
-import ResponsiveDemo from "./TailWindComponent/ResponsiveDemo";
-import WidthHeightDemo from "./TailWindComponent/WidthHeightDemo";
 
 
-
+//Này trong branch DuAnDatVe còn branch master để nữa
 function App() {
   return (
     <div className="">
