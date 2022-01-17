a9daebf81945eaf0e21186615466c502b1786b51
An <annogo123@gmail.com>
2022-01-17 10:07:52 +0700

    update_remove_all_folder_in_branch_master_in_branch_DuAnDatVe

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import "./App.css";
 
 
 //Này trong branch DuAnDatVe còn branch master để nữa
+//có gì thay đổi so với branch master thì add r commit cho nó update lại
 function App() {
   return (
     <div className="">
-      
+      123
     </div>
   );
 }
